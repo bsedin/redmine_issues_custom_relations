e2c390650f4939a4aa2a08ee3b13822c6e103204
Sergey Besedin <kr3ssh@gmail.com>
2014-04-09 22:02:13 +0400

    i18n, basic permissions

--- init.rb
+++ init.rb
@@ -11,4 +11,8 @@ Redmine::Plugin.register :issues_custom_relations do
   url 'http://github.com/kressh/redmine_issues_custom_relations'
   author_url 'http://github.com/kressh/redmine_issues_custom_relations'
   # settings :default => {'empty' => true}, :partial => 'settings/custom_relations_settings'
+  menu :admin_menu, :custom_relations_fields, { :controller => 'custom_relations_fields', :action => 'index' }, :caption => 'Custom Relations Fields'
+
+  permission :view_custom_relations_fields, :custom_relations_fields => :index
+  permission :edit_custom_relations_fields, :custom_relations_fields => :create
 end
