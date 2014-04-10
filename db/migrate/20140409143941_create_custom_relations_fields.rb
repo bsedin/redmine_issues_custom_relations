e2c1a8702f4a1fe5ecd78e97210088c397fd3358
Sergey Besedin <kr3ssh@gmail.com>
2014-04-10 12:33:26 +0400

    renamed migrations to time format

--- db/migrate/20140409143941_create_custom_relations_fields.rb
+++ db/migrate/20140409143941_create_custom_relations_fields.rb
@@ -2,8 +2,8 @@ class CreateCustomRelationsFields < ActiveRecord::Migration
   def change
     create_table :custom_relations_fields do |t|
       t.string :name, :null => false
-      t.integer :custom_field_id, :null => false # кастомное поле для поиска по нему задачи
-      t.integer :project_id, :null => false # проект, для которого включено custom relation поле
+      t.integer :custom_field_id, :null => false
+      t.integer :project_id, :null => false
     end
     add_index :custom_relations_fields, :custom_field_id
     add_index :custom_relations_fields, :project_id
