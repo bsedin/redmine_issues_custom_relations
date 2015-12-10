6c1f063cbeafc0622ce0e60da659c58e3564f5ff
Sergey Besedin <kr3ssh@gmail.com>
2015-12-11 00:36:21 +0300

    Bump version

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :issues_custom_relations do
   name 'Issues Custom Relations plugin'
   author 'Sergey Besedin'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.1.0'
   url 'http://github.com/kressh/redmine_issues_custom_relations'
   author_url 'http://github.com/kressh/redmine_issues_custom_relations'
   # settings :default => {'empty' => true}, :partial => 'settings/custom_relations_settings'
